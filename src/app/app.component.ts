3b5a24ff8451a16e6b8ba4c172574fe0e4a53014
Alex <42402960+mralexx11@users.noreply.github.com>
2019-09-03 19:12:02 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ohr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'money-saver';
+}
