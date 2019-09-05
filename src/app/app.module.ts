33a52b73a69bcf7fee2aa2bcbfba3f31b2f79ef9
Alex <42402960+mralexx11@users.noreply.github.com>
2019-09-05 16:58:22 +0300

    Basic components and modules for 'login' & 'registration' forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
